24e40e09f7a830cbb39a79e9599c460c0865678c
Trond Bråthen <tabrath@gmail.com>
2017-01-24 15:36:40 +0100

    hm

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 3001;
 
 app.listen(port, host, () => {
-  console.log('Listening on ' + host + ':' + port);
+  console.log('Listening on ' + host + ':' + port + ' and ready to rock');
 });
 
