08bf072a7b230fd5137affd4d54ce82190050f12
Trond Bråthen <tabrath@gmail.com>
2017-01-24 15:29:48 +0100

    minor changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello world, again.. trying some changes');
+  res.send('hello world, again.. trying some changes and I think its working!');
 });
 
 const host = process.env.HOST || 'localhost';
