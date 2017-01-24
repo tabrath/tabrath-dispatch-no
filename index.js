4939c502afc28315d4c00ac8f785a1adf6ea20de
tabrath <tabrath@gmail.com>
2017-01-24 14:00:28 +0000

    added host

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 3001;
+
+app.listen(port, host, () => {
+  console.log('listening on ' + host + ':' + port);
+});
+
