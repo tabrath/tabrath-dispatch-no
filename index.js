39c7dc7a45c0099a7122c089380a3f7be73316e9
Trond Bråthen <tabrath@gmail.com>
2017-01-25 15:55:48 +0100

    hmmz

--- index.js
+++ index.js
@@ -17,18 +17,10 @@ app.set('host', process.env.HOST || 'localhost');
 app.set('port', process.env.PORT || 3001);
 
 app.use('/vendor', express.static('./bower_components'));
-app.use('/', express.static('./dist'));
+app.use('/assets', express.static('./dist'));
+app.get('/*', (req, res) => res.sendFile(__dirname + '/dist/index.html'));
 
 //HACK: to get html5 mode to work we need everything else to route to the index.html
 var server = http.createServer(app);
-
-(function() {
-  var proxy = server.listen;
-  server.listen = function() {
-    app.get('/*', (req, res) => res.sendFile(__dirname + '/dist/index.html'));
-
-    return proxy.apply(this, arguments);
-  };
-})();
 
 module.exports = { server, app };
